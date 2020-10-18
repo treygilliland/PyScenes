49b5deb3fec72163320921ea00275868767d9fb6
treygilliland <jlg2266@columbia.edu>
2020-10-18 18:20:27 -0400

    Another hopeless docs commit

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
+import sphinx_rtd_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
@@ -21,18 +22,19 @@ copyright = '2020, Trey Gilliland'
 author = 'Trey Gilliland'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
 
-master_doc="index"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+master_doc = "index"
+
 extensions = ["recommonmark"]
-source_suffix = ['.rst', '.md']
+source_suffix = ['.md', '.rst']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -50,7 +52,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 
 # RTD Theme
-import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
