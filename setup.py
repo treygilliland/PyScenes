d86ce16ff3a179aeddd6879e4af8a60dd6c13238
treygilliland <jlg2266@columbia.edu>
2020-10-18 18:09:57 -0400

    Added pyscenes to PyPi

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name="pyscenes", packages=["pyscenes"])
+setup(name='PyScenes',
+      version='0.0.1',
+      description='PyScenes is a PyGame 2 wrapper for beginners.',
+      packages=['pyscenes'],
+      author_email='treygilliland3@gmail.com',
+      zip_safe=False)
