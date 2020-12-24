6c849b3c1aefe419a33d930f3634dd5259de8d51
treygilliland <jlg2266@columbia.edu>
2020-12-24 14:20:21 -0500

    Documented all examples

--- pyscenes/examples/scene_switcher.py
+++ pyscenes/examples/scene_switcher.py
@@ -13,6 +13,9 @@ class PrimaryScene(BaseScene):
     """
 
     def __init__(self, game, test=False):
+        """
+        Standard initialization method. Check to see if example was ran by testing class.
+        """
         print(f"Initializing {self.__repr__()}...")
         self.test = test
         if self.test:
@@ -22,28 +25,46 @@ class PrimaryScene(BaseScene):
         super().__init__(game)
 
     def __repr__(self):
+        """
+        Representation used for command line output for scene switch.
+        """
         return "PrimaryScene"
 
     def setup(self):
+        """
+        Output the scene name and change the background display.
+        """
         print(f"Setting up {self.__repr__()}...")
         self.game.display.background.setImage("assets/primary.png")
 
     def process_input(self, events, pressed_keys):
+        """
+        If the user presses the space bar, switch to the SecondaryScene.
+        """
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     self.switch_scene(SecondaryScene(self.game))
 
     def update(self):
+        """
+        Scenes are static.
+        """
         pass
 
     def render(self, display):
+        """
+        Scenes are static. Render count is used for PyScenes testing.
+        """
         if self.test:
             self.render_count += 1
             if self.render_count > 60:
                 super().terminate()
 
     def cleanup(self):
+        """
+        Output to show where in process cleanup occurs.
+        """
         print(f"Cleaning up {self.__repr__()}...")
 
 
@@ -53,6 +74,9 @@ class SecondaryScene(BaseScene):
     """
 
     def __init__(self, game, test=False):
+        """
+        Standard initialization method. Check to see if example was ran by testing class.
+        """
         print(f"Initializing {self.__repr__()}...")
         self.test = test
         if self.test:
@@ -62,28 +86,46 @@ class SecondaryScene(BaseScene):
         super().__init__(game)
 
     def __repr__(self):
+        """
+        Representation used for command line output for scene switch.
+        """
         return "SecondaryScene"
 
     def setup(self):
+        """
+        Output the scene name and change the background display.
+        """
         print(f"Setting up {self.__repr__()}...")
         self.game.display.background.setImage("assets/secondary.png")
 
     def process_input(self, events, pressed_keys):
+        """
+        If the user presses the space bar, switch to the SecondaryScene.
+        """
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     self.switch_scene(PrimaryScene(self.game))
 
     def update(self):
+        """
+        Scenes are static.
+        """
         pass
 
     def render(self, display):
+        """
+        Scenes are static. Render count is used for PyScenes testing.
+        """
         if self.test:
             self.render_count += 1
             if self.render_count > 60:
                 super().terminate()
 
     def cleanup(self):
+        """
+        Output to show where in process cleanup occurs.
+        """
         print(f"Cleaning up {self.__repr__()}...")
 
 
